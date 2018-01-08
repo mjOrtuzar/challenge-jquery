359608fa5199725bc8c78ff74135d641c3856fc3
mjOrtuzar <re4ortuzar@hotmail.com>
2018-01-08 17:15:32 -0300

    Etapa 4: se prepara div dinamico con el nombre e imagenes de las recetas

--- js/script-index.js
+++ js/script-index.js
@@ -18,11 +18,10 @@ $(document).ready(function printNews(){
 */
 function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
-	var size = recipesArray.length;
-	for (i =0 ; i<=size ;i++){
-		if(recipesArray[i].highlighted){
-			renderRecipe(recipesArray[i]);
-			/*console.log(recipesArray[i].title);*/
+	var size = recipesArray.length;  // se crea una variable que almacena el largo del array
+	for (i =0 ; i<=size ;i++){   // el for itera a lo largo del array para luego abrir su condicion
+		if(recipesArray[i].highlighted){  // si la propiedad llamada desde el indice del array es verdad
+			renderRecipe(recipesArray[i]);  //se llama a la funcion renderRecipe colocando como parametro, el contenido del indice que resulto verdadero.
 		}
 	}
 
@@ -34,8 +33,20 @@ function renderHighlightedRecipes(recipesArray) {
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
 */
-function renderRecipe(recipe) {
+function renderRecipe(recipe) { //lo que se da como parametro es el resultado de renderHighlightedRecipes(recipesArray) que contiene el array con el que se trabajara.
 	console.log('Voy a pintar la receta: ', recipe);
+	// se toma el div vacio con clase list-recipes y se añaden etiquetas html.
+	//para que sea dinamico, en los titulos se coloca el nombre del array y su metodo para ingresar a los objetos que contiene.
+	$(".list-recipes").append('<a class="item-recipe" href="#">'+
+	'<span class="attribution">'+
+	'<span class="title-recipe">'+ recipe.title +'</span>'+
+	'<span class="metadata-recipe">'+
+	'<span class="author-recipe">'+ recipe.source.name +'</span>'+
+	'<span class="bookmarks-recipe">'+
+	'<span class="icon-bookmark"></span>'+
+	'</span>'+'</span>'+'</span>'+
+	'<img src="img/recipes/640x480/' + recipe.name + '.jpg"/>'+'</a>');
+
 }
 
 
